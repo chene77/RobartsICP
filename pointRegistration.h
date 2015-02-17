63b451d7578f68375b5857dba13a86fe28ff9699
Elvis Chen <chene.rri@gmail.com>
2015-02-17 09:43:45 -0500

    Create pointRegistration.h

---
+++ pointRegistration.h
@@ -0,0 +1,53 @@
+/*=========================================================================
+
+Program:   Robarts Computer Vision
+Module:    $RCSfile: pointRegistration.h,v $
+Creator:   Elvis C. S. Chen <chene@robarts.ca>
+Language:  C++
+Author:    $Author: Elvis Chen $
+Date:      $Date: 2013/09/09 17:07:30 $
+Version:   $Revision: 0.99 $
+
+==========================================================================
+
+This is an open-source copyright license based on BSD 2-Clause License,
+see http://opensource.org/licenses/BSD-2-Clause
+
+Copyright (c) 2013, Elvis Chia-Shin Chen
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+#ifndef __ASOPP_H__
+#define __ASOPP_H__
+
+#include "matrix.h"
+
+// ordinary solution of Orthogonal Procrustes Problem using SVD
+void point_register( Matrix<double> &x, Matrix<double> &y,
+			         Matrix<double> &Q,
+			         Vec<double> &t,
+			         double &FRE, double threshold,
+                     Vec<double> &FREMag );
+
+#endif // of __ASOPP_MAJOR_H__
